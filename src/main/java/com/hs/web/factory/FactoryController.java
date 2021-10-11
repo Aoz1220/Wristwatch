7cc5ec3721c5d93c48584129cc0e4cb2b62ca444
林澳 <1160837670@qq.com>
2021-10-11 21:14:36 +0800

    腕表管理审核、拒绝维修、下放到维修厂的完成与完善，维修厂待维修页面完成

---
+++ src/main/java/com/hs/web/factory/FactoryController.java
@@ -0,0 +1,67 @@
+package com.hs.web.factory;
+
+import com.github.pagehelper.PageInfo;
+import com.hs.common.Constants;
+import com.hs.model.*;
+import com.hs.service.UserService;
+import com.hs.service.WatchService;
+import com.hs.utils.SessionUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/factory")
+public class FactoryController {
+
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private WatchService watchService;
+
+    /**
+     * 跳转到待维修腕表列表页面
+     * @return
+     */
+    @RequestMapping("/watch/waiting")
+    public String toWatchWaiting(Model model){
+        User user=(User) SessionUtil.getPrimaryPrincipal();
+        List<Brand> brandListAll=userService.getBrandByTypeId(user.getTypeId());
+        model.addAttribute("brandListAll",brandListAll);
+        return "factory/watch-list-waiting";
+    }
+
+    /**
+     * 待修理列表数据查询
+     * @param watchname
+     * @param username
+     * @param brandId
+     * @param page
+     * @param limit
+     * @return
+     */
+    @RequestMapping("/watch/waiting/data.json")
+    @ResponseBody
+    public Map getWatchWaitingData(String watchname, String username, Integer brandId, Integer page, Integer limit){
+        Map map=new HashMap();
+        //获取登录人修理腕表类型ID
+        User user= (User)SessionUtil.getPrimaryPrincipal();
+        //查询该类型腕表列表
+        List<Watch> list=watchService.getWatchList(user.getTypeId(),watchname,username,brandId, Constants.RESIDENT_STATUS_2,page,limit);
+        PageInfo<Watch> pageInfo=new PageInfo<Watch>(list);
+        //封装返回接口
+        map.put("code","0");
+        map.put("msg","");
+        map.put("count",pageInfo.getTotal());
+        map.put("data",pageInfo.getList());
+        return map;
+    }
+}
