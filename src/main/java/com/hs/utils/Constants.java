6f9b5e65f1ab1cc284279e5b33d0c75543dea581
倪梓然 <1942909451@qq.com>
2021-10-13 21:01:34 +0800

    客户维修订单与已完成订单页面1.1

--- src/main/java/com/hs/utils/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.hs.utils;
-
-public class Constants {
-
-	public static final String CON_cust_source="cust_source";
-	public static final String CON_cust_level="cust_level";
-	public static final String CON_cust_industry="cust_industry";
-}
